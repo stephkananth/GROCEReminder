b9c4789f06a6a5e4ddbe013629e5eae3d379a991
Ameeshi Goel <ameeshig@andrew.cmu.edu>
2018-10-30 17:20:09 -0400

    first commit

---
+++ GROCEReminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GROCEReminder
+//
+//  Created by Ameeshi Goel on 10/30/18.
+//  Copyright © 2018 Ameeshi Goel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
