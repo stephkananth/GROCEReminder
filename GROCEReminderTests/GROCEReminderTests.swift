7d27a1a0961d9ae3f66c8d76d079fa8df8cb392a
stephkananth <steph.ananth@gmail.com>
2018-12-06 18:27:32 -0500

    model test done; still have to do view models; progress is being made very slowly

--- GROCEReminderTests/GROCEReminderTests.swift
+++ GROCEReminderTests/GROCEReminderTests.swift
@@ -19,10 +19,16 @@ class GROCEReminderTests: XCTestCase {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
   }
   
-  func testItem() {
-    let test_item = Item(name: "Milk", location: "fridge", purchase_date: Date(), expiration_date: Date())
-    XCTAssert(test_item.name == "Milk")
-    XCTAssert(test_item.location == "fridge")
+  func test_Item() {
+    let testItem = Item(name: "Milk", location: "fridge", purchase_date: Date(), expiration_date: Date())
+    XCTAssert(testItem.name == "Milk")
+    XCTAssert(testItem.location == "fridge")
   }
   
+  func test_ShelfItem() {
+    let testShelfItem = ShelfItem(name: "Milk", category_num: 7, pantry_length: -1, pantry_metric: "", fridge_length: 2, fridge_metric: "Weeks", freeze_length: -1, freeze_metric: "")
+    XCTAssert(testShelfItem.name == "Milk")
+    XCTAssert(testShelfItem.category == "Dairy Products & Eggs")
+    XCTAssert(testShelfItem.location == "fridge")
+  }
 }
